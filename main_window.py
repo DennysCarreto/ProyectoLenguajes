0b227c8d871aa25538918950733818d3a773701f
Ruben-online <78996839+Ruben-online@users.noreply.github.com>
2024-09-11 17:59:00 -0600

    Agrege el menu que cree en "menu.py"

--- main_window.py
+++ main_window.py
@@ -1,4 +1,6 @@
 from PyQt6.QtWidgets import QMainWindow
+from menu import CreateMenuBar
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -7,3 +9,11 @@ class MainWindow(QMainWindow):
         # Ventana principal
         self.setWindowTitle("Proyecto Compilador")
         self.setGeometry(400, 100, 800, 600)
+
+        # Crear la barra de menú utilizando la clase CreateMenuBar
+        menu_creator = CreateMenuBar(self)
+        menu_creator.file_menu()  # Agrega la opcion "Archivo"
+        menu_creator.exit_menu()  # Agrega la opcion "Salir"
+
+        # Configurar la barra de menú
+        self.setMenuBar(menu_creator.get_menu_bar())
