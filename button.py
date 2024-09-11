ed70304f81f9576c9c0a567e6b4b770f4eb1ce30
Ruben-online <78996839+Ruben-online@users.noreply.github.com>
2024-09-11 17:57:39 -0600

    Cree la clase "CustomButton" para su posterior uso en el proyecto

---
+++ button.py
@@ -0,0 +1,9 @@
+from PyQt6.QtWidgets import QPushButton
+
+class CustomButton(QPushButton):
+    def __init__(self, text, parent=None):
+        super().__init__(text, parent)
+
+        # Boton
+        self.setText(text)
+        self.setFixedSize(100, 50)
