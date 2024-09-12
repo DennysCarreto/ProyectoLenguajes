d4b29a21765012b850d0bd219e4f0a0ce4b8f11c
Ruben-online <78996839+Ruben-online@users.noreply.github.com>
2024-09-11 18:47:02 -0600

    Agrege métodos para la creación del menú

--- menu.py
+++ menu.py
@@ -12,10 +12,32 @@ class CreateMenuBar:
         file_menu = QMenu("Archivo", self.window)
         self.menu_bar.addMenu(file_menu)
 
-        # Crear acciones dentro del menú "Archivo"
-        open_action = QAction("Abrir", self.window)
+        # Crear acciones dentro de "Archivo"
+        open_action = QAction("Cargar un archivo", self.window)
         file_menu.addAction(open_action)
         file_menu.addSeparator()
+
+    def analysis_menu(self):
+        # Crear el menú "Análisis"
+        analysis_menu = QMenu("Análisis", self.window)
+        self.menu_bar.addMenu(analysis_menu)
+
+        # Crear acciones dentro de "Análisis"
+        star_analysis_action = QAction("Iniciar análisis", self.window)
+        analysis_menu.addAction(star_analysis_action)
+        stop_analysis_action = QAction("Detener análisis", self.window)
+        analysis_menu.addAction(stop_analysis_action)
+        analysis_menu.addSeparator()
+
+    def tokens_menu(self):
+        # Crear el menú "Tokens"
+        tokens_menu = QMenu("Tokens", self.window)
+        self.menu_bar.addMenu(tokens_menu)
+
+        # Crear acciones dentro de "Tokens"
+        see_tokens_action = QAction("Ver tokens", self.window)
+        tokens_menu.addAction(see_tokens_action)
+        tokens_menu.addSeparator()
 
     def exit_menu(self):
         # Crear el menú "Salir"
